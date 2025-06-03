cd40ff3b714501a9c7dc3962310aafddac7ce405
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-06-03 14:03:12 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,9 +1,11 @@
 // Add the required props
-import { UserInfo } from '../UserInfo/UserInfo';
+import { UserInfo } from '../UserInfo';
 
 export const TodoInfo = ({ todo }) => {
   return (
-    <article className={`TodoInfo ${todo.completed && 'TodoInfo--completed'}`}>
+    <article
+      className={`TodoInfo ${todo.completed ? 'TodoInfo--completed' : ''}`}
+    >
       <h2 className="TodoInfo__title">{todo.title}</h2>
       {todo.user && <UserInfo user={todo.user} />}
     </article>
