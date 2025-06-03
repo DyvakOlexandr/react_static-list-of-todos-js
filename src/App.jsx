9ed2f761650aa1e5e2b4d3aadce276ad4a33de48
DyvakOleksandr <dyvakaleksandr370@gmail.com>
2025-06-03 13:35:33 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss';
 
 import todosFromServer from './api/todos.json';
 import usersFromServer from './api/users.json';
+import { TodoList } from './components/TodoList';
 
 function getUserById(userId) {
   return usersFromServer.find(user => user.id === userId) || null;
@@ -49,5 +50,6 @@ export const App = () => (
         </a>
       </article>
     </section>
+    <TodoList todos={todos} />
   </div>
 );
